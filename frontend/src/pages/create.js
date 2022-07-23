d8233b5a1ef24dd94e48c10e78e75f76d6d17b91
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2022-07-23 18:54:51 +0100

    Create hackathon page

--- frontend/src/pages/create.js
+++ frontend/src/pages/create.js
@@ -66,14 +66,23 @@ const Create = () => {
     const [description, setDescription] = useState('')
     const [startDate, setStartDate] = useState('')
     const [endDate, setEndDate] = useState('')
-    const [prizes, setPrizes] = useState([])
+    const [prizes, setPrizes] = useState([{ name: '', value: '' }])
+    const [moments, setMoments] = useState([])
 
-    const addOption = () => {
+    const addPrize = () => {
         setPrizes([...prizes, { name: '', value: '' }])
     }
 
-    const removeOption = (index) => {
+    const addMoment = () => {
+        setMoments([...moments, { title: '', timestamp: '' }])
+    }
+
+    const removePrize = (index) => {
         setPrizes(prizes.filter((_, i) => i !== index))
+    }
+
+    const removeMoment = (index) => {
+        setMoments(moments.filter((_, i) => i !== index))
     }
 
     const submitButton = {
@@ -108,35 +117,73 @@ const Create = () => {
                             <td style={labelStyle} colSpan='1'>
                                 <label>Title</label>
                             </td>
-                            <td colSpan='3' >
-                                <input style={inputStyle} placeholder="e.g. Pasteis" onChange={(e) => setName(e.target.value) } />
+                            <td colSpan='5' >
+                                <input style={inputStyle} placeholder="e.g. Pasteis" onChange={(e) => setName(e.target.value)} />
                             </td>
                         </tr>
                         <tr>
                             <td style={labelStyle} colSpan='1'>
                                 <label>Description</label>
                             </td>
-                            <td colSpan='3'>
+                            <td colSpan='5'>
                                 <textarea style={inputStyle} placeholder="e.g. Pasteis is a hackathon that..." rows="10" onChange={(e) => setDescription(e.target.value)} />
                             </td>
                         </tr>
+                        {/* Dates */}
                         <tr>
                             <td colSpan='1' style={labelStyle}>Start date</td>
-                            <td colSpan='1'><input type="date" onChange={(e) => setStartDate(e.target.value)} /></td>
-                            <td colSpan='1' style={labelStyle}>End date</td>
-                            <td colSpan='1'><input type="date" onChange={(e) => setEndDate(e.target.value)}/></td>
+                            <td colSpan='5'><input type="date" style={inputStyle} onChange={(e) => setStartDate(e.target.value)} /></td>
                         </tr>
                         <tr>
-                            <td colSpan='4' style={{
-                                textAlign: 'center',
-
-                            }}>Prizes</td>
-
+                            <td colSpan='1' style={labelStyle}>End date</td>
+                            <td colSpan='5'><input type="date" style={inputStyle} onChange={(e) => setEndDate(e.target.value)} /></td>
                         </tr>
+
+                        {/* Custom number of moments available */}
                         {
+                            moments.map((moment, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td colSpan="1" style={labelStyle}>Moment #{index + 1}</td>
+                                        <td colSpan='2'>
+                                            <input style={inputStyle} placeholder="e.g. First check-in"
+                                                value={moment.title}
+                                                onChange={(e) => {
+                                                    moments[index].title = e.target.value
+                                                    setMoments([...moments])
+                                                }}
+                                            />
+                                        </td>
+                                        <td colSpan='2'>
+                                            <input style={inputStyle} type="date"
+                                                value={moment.value}
+                                                onChange={(e) => {
+                                                    moments[index].value = e.target.value
+                                                    setMoments([...moments])
+                                                }}
+                                            />
+                                        </td>
+                                        <td colSpan='1'><button style={minusButtonStyle} onClick={() => removeMoment(index)}>-</button></td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        <tr>
+                            <td colSpan='6' style={addPrizeStyle}>
+                                <div style={plusButtonStyle} onClick={() => addMoment()}>
+                                    +
+                                </div>
+                                <label>Add moments</label>
+                            </td>
+                        </tr>
+
+
+                        {
+                            // Prizes
                             prizes.map((prize, index) => {
                                 return (
                                     <tr key={index}>
+                                        <td colSpan="1" style={labelStyle}>Prize #{index + 1}</td>
                                         <td colSpan='2'>
                                             <input style={inputStyle} placeholder="e.g. First prize"
                                                 value={prize.name}
@@ -146,7 +193,7 @@ const Create = () => {
                                                 }}
                                             />
                                         </td>
-                                        <td colSpan='1'>
+                                        <td colSpan='2'>
                                             <input style={inputStyle} type="number" placeholder="e.g. 100"
                                                 value={prize.value}
                                                 onChange={(e) => {
@@ -155,17 +202,17 @@ const Create = () => {
                                                 }}
                                             />
                                         </td>
-                                        <td colSpan='1'><button style={minusButtonStyle} onClick={() => removeOption(index)}>-</button></td>
+                                        <td colSpan='1'><button style={minusButtonStyle} onClick={() => removePrize(index)}>-</button></td>
                                     </tr>
                                 )
                             })
                         }
-
                         <tr>
-                            <td colSpan='4' style={addPrizeStyle}>
-                                <div style={plusButtonStyle} onClick={() => addOption()}>
+                            <td colSpan='6' style={addPrizeStyle}>
+                                <div style={plusButtonStyle} onClick={() => addPrize()}>
                                     +
                                 </div>
+                                <label>Add prize</label>
                             </td>
                         </tr>
 
