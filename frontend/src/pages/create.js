1a7ef21451087ea0fd2313be430e7ebae82693d2
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2022-07-23 13:14:59 +0100

    Create hackathon page

--- frontend/src/pages/create.js
+++ frontend/src/pages/create.js
@@ -7,69 +7,174 @@ const Create = () => {
     const header_items = [
         { content: 'Home', link: '/home' },
         { content: 'Explore', link: '/explore' },
-        { content: 'Create', link: '/create',  selected: true},
+        { content: 'Create', link: '/create', selected: true },
         { content: 'About', link: '/about' },
         { content: 'Username', link: '/user' }, // TODO we need to add the circle with the avatar here. can simply be put in the content
     ];
 
-    const titleStyle = {
-        fontSize: '36px',
-    }
-
     const listStyle = {
         display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        margin: 'auto',
-        width: '90%',
-        textAlign: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    }
+
+    const labelStyle = {
+        fontSize: '18px',
+        textAlign: 'right',
+        paddingRight: '20px',
+        verticalAlign: 'top',
     }
 
     const inputStyle = {
-        width: '50%',
-        marginTop: '-20px',
-        marginBottom: '20px',
+        width: '90%',
         backgroundColor: '#EDEFF2',
         border: 'none',
         borderRadius: '5px',
         fontSize: '18px',
-        height: '70px',
         paddingLeft: '20px',
     }
 
-    const formStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+    const addPrizeStyle = {
         textAlign: 'center',
-        backgroundColor: 'red',
-        flexGrow: 1,
+
     }
 
-    const buttonStyle = {
+    const plusButtonStyle = {
+        backgroundColor: 'black',
+        color: 'white',
+        borderRadius: '100%',
+        width: '50px',
+        height: '50px',
+        fontSize: '32px',
+        cursor: 'pointer',
+        margin: 'auto',
+    }
+
+    const minusButtonStyle = {
+        backgroundColor: 'black',
+        color: 'white',
+        borderRadius: '100%',
+        width: '25px',
+        height: '25px',
+        fontSize: '16px',
+        cursor: 'pointer',
+        margin: 'auto',
+    }
+
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [startDate, setStartDate] = useState('')
+    const [endDate, setEndDate] = useState('')
+    const [prizes, setPrizes] = useState([])
+
+    const addOption = () => {
+        setPrizes([...prizes, { name: '', value: '' }])
+    }
+
+    const removeOption = (index) => {
+        setPrizes(prizes.filter((_, i) => i !== index))
+    }
+
+    const submitButton = {
         backgroundColor: 'black',
         color: 'white',
         border: 'none',
-        borderRadius: '10px',
+        borderRadius: '15px',
         fontSize: '16px',
-        padding: '8px',
+        padding: '10px',
         cursor: 'pointer',
-        margin: '10px'
     }
 
+    const submitStyle = {
+        margin: 'auto',
+        width: '300px',
+        textAlign: 'center',
+    }
+
+    const submitFn = () => {
+        console.log('Submit')
+        console.log(name, description, startDate, endDate, prizes)
+
+    }
 
     return (
         <>
             <Header options={header_items} />
             <div style={listStyle}>
-                <p style={titleStyle}>Create a hackathon</p>
+                <table style={{ borderSpacing: '20px' }}>
+                    <tbody>
+                        <tr>
+                            <td style={labelStyle} colSpan='1'>
+                                <label>Title</label>
+                            </td>
+                            <td colSpan='3' >
+                                <input style={inputStyle} placeholder="e.g. Pasteis" onChange={(e) => setName(e.target.value) } />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={labelStyle} colSpan='1'>
+                                <label>Description</label>
+                            </td>
+                            <td colSpan='3'>
+                                <textarea style={inputStyle} placeholder="e.g. Pasteis is a hackathon that..." rows="10" onChange={(e) => setDescription(e.target.value)} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan='1' style={labelStyle}>Start date</td>
+                            <td colSpan='1'><input type="date" onChange={(e) => setStartDate(e.target.value)} /></td>
+                            <td colSpan='1' style={labelStyle}>End date</td>
+                            <td colSpan='1'><input type="date" onChange={(e) => setEndDate(e.target.value)}/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan='4' style={{
+                                textAlign: 'center',
 
-                <input style={inputStyle} placeholder="name" onChange={()=>{}} />
-                <input style={inputStyle} placeholder="description" onChange={()=>{}} />
-                <input style={inputStyle} placeholder="date" onChange={()=>{}} />
-                <input style={inputStyle} placeholder="total prize (in ETH)" onChange={()=>{}} />
-                <button style={buttonStyle}>Upload Image</button>
-                <button style={buttonStyle}>Submit</button>
+                            }}>Prizes</td>
+
+                        </tr>
+                        {
+                            prizes.map((prize, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td colSpan='2'>
+                                            <input style={inputStyle} placeholder="e.g. First prize"
+                                                value={prize.name}
+                                                onChange={(e) => {
+                                                    prizes[index].name = e.target.value
+                                                    setPrizes([...prizes])
+                                                }}
+                                            />
+                                        </td>
+                                        <td colSpan='1'>
+                                            <input style={inputStyle} type="number" placeholder="e.g. 100"
+                                                value={prize.value}
+                                                onChange={(e) => {
+                                                    prizes[index].value = e.target.value
+                                                    setPrizes([...prizes])
+                                                }}
+                                            />
+                                        </td>
+                                        <td colSpan='1'><button style={minusButtonStyle} onClick={() => removeOption(index)}>-</button></td>
+                                    </tr>
+                                )
+                            })
+                        }
+
+                        <tr>
+                            <td colSpan='4' style={addPrizeStyle}>
+                                <div style={plusButtonStyle} onClick={() => addOption()}>
+                                    +
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div style={submitStyle}>
+                <button style={submitButton} onClick={submitFn}>Create hackathon</button>
             </div>
         </>
     );
